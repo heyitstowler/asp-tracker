7b0623f142ea83ea02f560874224a17edd3ad404
Chris Towler <ctowler.518@gmail.com>
2020-11-03 09:03:30 -0500

    Revert "Try passing args to get rid of puppeteer error"
    This reverts commit c22589265348edc2e9cacc3bd5af4afa49c55f28.

--- pages/api/scrape.js
+++ pages/api/scrape.js
@@ -1,28 +1,9 @@
 import puppeteer from "puppeteer";
 
 export async function scrape() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: [
-      "--disable-gpu",
-      "--disable-dev-shm-usage",
-      "--disable-setuid-sandbox",
-      "--timeout=30000",
-      "--no-first-run",
-      "--no-sandbox",
-      "--no-zygote",
-      "--single-process",
-      "--proxy-server='direct://'",
-      "--proxy-bypass-list=*",
-      "--deterministic-fetch",
-    ],
-  });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.setUserAgent(
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36"
-  );
   await page.goto("https://portal.311.nyc.gov/");
-  await page.setViewport({ width: 1680, height: 1050 });
   const data = await page.evaluate(() => {
     const status = document.getElementsByClassName("cal-alert")[0].innerHTML;
     return {
