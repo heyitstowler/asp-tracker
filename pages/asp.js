d7b046db0545dcef59ac90bf5f0fa642086207d6
gregliuzzi@gmail.com <greg@Gregs-MacBook.home>
2020-11-05 00:31:55 -0500

    added styling and a link to NYCASP twitter

--- pages/asp.js
+++ pages/asp.js
@@ -1,17 +1,20 @@
 import { scrape } from "./api/scrape";
-import Layout from '../components/layout';
+import Content from '../components/content';
+import Header from '../components/header';
+import styles from '../components/asp.module.css'
 
 export default function AspPage(props) {
   const {
     data: { status },
   } = props;
   return (
-    <Layout>
-      <main>
-        <h1>Current ASP Status</h1>
-        <h2>{status}</h2>
-      </main>
-    </Layout>
+    <div>
+        <Content>
+            <h1>Current ASP Status</h1>
+            <h2 className={status==='IN EFFECT' ? styles.asp : null}>{status}</h2>
+            <h3><a href="https://twitter.com/NYCASP?s=20">NYCASP Twitter</a></h3>
+        </Content>
+    </div>
   );
 }
 
